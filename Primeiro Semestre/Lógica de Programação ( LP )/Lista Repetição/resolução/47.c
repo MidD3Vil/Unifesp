0fb98dd3453f8d351629ae21376d6269444c238f
MidD3Vil <coralvinicius3110@gmail.com>
2026-04-13 16:45:49 -0300

    update 47.c

---
+++ Primeiro Semestre/Lógica de Programação ( LP )/Lista Repetição/resolução/47.c
@@ -0,0 +1,25 @@
+// 47)
+#include <stdio.h>
+
+int main() {
+   float num, divi;
+   float den = 0;
+   
+   printf("Digite o numerador: ");
+   scanf("%f", &num);
+   
+   while(1) {
+      printf("\nDigite o denominador: ");
+      scanf("%f", &den);
+      if(den!=0) {
+          break;
+      }    
+      else {
+          printf("\nVALOR INVÁLIDO");
+      }
+   }
+   
+   divi = num/den;
+   printf("A divisão de %.2f por %.2f é igual a %.2f", num, den, divi);
+   return 0;
+}
