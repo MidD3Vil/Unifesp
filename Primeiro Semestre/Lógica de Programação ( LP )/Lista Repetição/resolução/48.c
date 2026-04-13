1970a0a276bd227b4f0eaef3417a9696ef84aa90
MidD3Vil <coralvinicius3110@gmail.com>
2026-04-13 18:39:14 -0300

    update 48.c

---
+++ Primeiro Semestre/Lógica de Programação ( LP )/Lista Repetição/resolução/48.c
@@ -0,0 +1,29 @@
+// 48)
+#include <stdio.h>
+
+int main() {
+    float n1, n2;
+    
+    while(1) {
+        printf("\nNota da primeira prova: ");
+        scanf("%f", &n1);
+        if(0<=n1 && n1<=10) {
+            break;
+        }
+        else {
+            printf("A nota deve estar entre 0-10...");
+        }
+    }
+    while(1) {
+        printf ("\nNota da segunda prova: ");
+        scanf("%f", &n2);
+        if(0<=n2 && n2<=10) {
+            break;
+        }
+        else {
+            printf("A nota deve estar entre 0-10...");
+        }    
+    }
+    printf("A média simples entre as duas notas é %.2f", (n1+n2)/2);
+    return 0;
+}
