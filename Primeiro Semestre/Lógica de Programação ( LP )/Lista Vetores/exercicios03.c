f85396eb50db86b2dcffb29eb8cff1a3d880c757
MidD3Vil <coralvinicius3110@gmail.com>
2026-04-23 15:46:59 -0300

    update

---
+++ Primeiro Semestre/Lógica de Programação ( LP )/Lista Vetores/exercicios03.c
@@ -0,0 +1,42 @@
+/*
+79) Escreva um algoritmo que permita a leitura das notas de uma turma de 20 alunos. Calcular a média 
+da turma e contar quantos alunos obtiveram nota acima desta média calculada. Escrever a média da 
+turma e o resultado da contagem.
+
+80) Ler um vetor Q de 20 posições (aceitar somente números positivos). Escrever a seguir o valor do 
+maior elemento de Q e a respectiva posição que ele ocupa no vetor.
+
+81) O mesmo exercício anterior, mas agora deve escrever o menor elemento do vetor e a respectiva 
+posição dele nesse vetor.
+
+82) Ler um vetor A de 10 números. Após, ler mais um número e guardar em uma variável X. 
+Armazenar em um vetor M o resultado de cada elemento de A multiplicado pelo valor X. Logo após, 
+imprimir o vetor M.
+
+83) Faça um algoritmo para ler 20 números e armazenar em um vetor. Após a leitura total dos 20 
+números, o algoritmo deve escrever esses 20 números lidos na ordem inversa. 
+
+84) Faça um algoritmo para ler um valor N qualquer (que será o tamanho dos vetores). Após, ler dois 
+vetores A e B (de tamanho N cada um) e depois armazenar em um terceiro vetor Soma a soma dos 
+elementos do vetor A com os do vetor B (respeitando as mesmas posições) e escrever o vetor Soma. 
+
+85) Faça um algoritmo para ler e armazenar em um vetor a temperatura média de todos os dias do ano. 
+Calcular e escrever: 
+a) Menor temperatura do ano 
+b) Maior temperatura do ano 
+c) Temperatura média anual 
+d) O número de dias no ano em que a temperatura foi inferior a média anual 
+
+88) Faça um algoritmo para ler um vetor de 20 números. Após isto, deverá ser lido mais um número 
+qualquer e verificar se esse número existe no vetor ou não. Se existir, o algoritmo deve gerar um novo 
+vetor sem esse número. (Considere que não haverão números repetidos no vetor). 
+
+89) Faça um algoritmo para ler dois vetores V1 e V2 de 15 números cada. Calcular e escrever a 
+quantidade de vezes que V1 e V2 possuem os mesmos números e nas mesmas posições. 
+
+90) Faça um algoritmo para ler um vetor de 30 números. Após isto, ler mais um número qualquer, 
+calcular e escrever quantas vezes esse número aparece no vetor. 
+
+91) Faça um algoritmo para ler 50 números e armazenar em um vetor VET, verificar e escrever se 
+existem números repetidos no vetor VET e em que posições se encontram. 
+*/
