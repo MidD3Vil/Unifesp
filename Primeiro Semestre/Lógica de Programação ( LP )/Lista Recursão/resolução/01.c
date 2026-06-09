fb2771e3dfce1a62e0a2373d00761c79f040b74b
Vinícius Coral <79802337+MidD3Vil@users.noreply.github.com>
2026-06-08 23:11:07 -0300

    Update 01.c

--- Primeiro Semestre/Lógica de Programação ( LP )/Lista Recursão/resolução/01.c
+++ Primeiro Semestre/Lógica de Programação ( LP )/Lista Recursão/resolução/01.c
@@ -1,1 +1,20 @@
+// Ex 1
+#include <stdio.h>
 
+int soma(int n) {
+    if (n == 1)
+        return 1;
+
+    return n + soma(n - 1);
+}
+
+int main() {
+    int n;
+
+    printf("Digite um numero positivo: ");
+    scanf("%d", &n);
+
+    printf("Somatorio = %d\n", soma(n));
+
+    return 0;
+}
