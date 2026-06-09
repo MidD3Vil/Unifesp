5b7776a0794700e4f006b4a816950e4d65aad556
Vinícius Coral <79802337+MidD3Vil@users.noreply.github.com>
2026-06-08 23:13:13 -0300

    Add recursive function to print numbers in descending order

---
+++ Primeiro Semestre/Lógica de Programação ( LP )/Lista Recursão/resolução/08.c
@@ -0,0 +1,22 @@
+// Ex 8
+#include <stdio.h>
+
+void decrescente(int n) {
+    if (n < 0)
+        return;
+
+    printf("%d ", n);
+
+    decrescente(n - 1);
+}
+
+int main() {
+    int n;
+
+    printf("Digite N: ");
+    scanf("%d", &n);
+
+    decrescente(n);
+
+    return 0;
+}
